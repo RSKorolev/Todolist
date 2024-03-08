d7affefb002dded16b492ea1361859d76a244d5f
Ruslan Korolev <RSKorolev@bk.ru>
2024-03-09 01:14:18 +0700

    code refactoring

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,47 +1,45 @@
-import { Button } from "./Button";
+import { Button } from './Button';
 
 export type TodoListPropsType = {
-  title: string;
-  tasks: TaskType[];
-  removeTask: (taskId: number) => void;
+    title: string;
+    tasks: TaskType[];
+    removeTask: (taskId: number) => void;
 };
 
 export type TaskType = {
-  id: number;
-  title: string;
-  isDone: boolean;
+    id: number;
+    title: string;
+    isDone: boolean;
 };
 
 export const TodoList = ({ title, tasks, removeTask }: TodoListPropsType) => {
-  const tasksList: JSX.Element =
-    tasks.length === 0 ? (
-      <span>Список пуст</span>
-    ) : (
-      <ul>
-        {tasks.map((task: TaskType) => {
-          return (
-            <li key={task.id}>
-              <input type="checkbox" checked={task.isDone} />
-              <span>{task.title}</span>
-              <button onClick={() => removeTask(task.id)}>x</button>
-            </li>
-          );
-        })}
-      </ul>
+    const tasksList =
+        tasks.length === 0 ? (
+            <span>Нет задач</span>
+        ) : (
+            tasks.map((task) => {
+                return (
+                    <li key={task.id}>
+                        <input type="checkbox" checked={task.isDone} />
+                        <span>{task.title}</span>
+                        <button onClick={() => removeTask(task.id)}>x</button>
+                    </li>
+                );
+            })
+        );
+    return (
+        <div className="todolist">
+            <h3>{title}</h3>
+            <div>
+                <input />
+                <Button title={'+'} />
+            </div>
+            <ul>{tasksList}</ul>
+            <div>
+                <Button title={'All'} />
+                <Button title={'Active'} />
+                <Button title={'Completed'} />
+            </div>
+        </div>
     );
-  return (
-    <div className="todolist">
-      <h3>{title}</h3>
-      <div>
-        <input />
-        <Button title="+" />
-      </div>
-      <ul>{tasksList}</ul>
-      <div>
-        <Button title="All" />
-        <Button title="Active" />
-        <Button title="Completed" />
-      </div>
-    </div>
-  );
 };
